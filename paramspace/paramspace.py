a2b3a0c0ae1e41a3d15caf1929217f0d662297d2
Tom Bland <23723407+tsmbland@users.noreply.github.com>
2023-01-23 16:56:49 +0000

    reformatted docstrings, type hints

--- paramspace/paramspace.py
+++ paramspace/paramspace.py
@@ -3,96 +3,51 @@ import os
 import multiprocessing
 import matplotlib.pyplot as plt
 import shutil
-
-"""
-To do: function to link boundary points
-
-"""
-
-
-class ParamSpace1D:
-    def __init__(self, func, p_vals, direc, parallel=False, cores=None):
-        """
-        Runs func with all combinations of p1_vals and p2_vals, saves results to csv file
-
-        :param func: function, takes two parameter values, returns a float
-        :param p_vals: array of parameter values
-        :param direc: directory to save results
-        :param parallel: if True, will run in parallel using number of cores specified
-        :param cores: number of cores to use, if parallel=True
-
-        To do:
-        - check that this works
-        - ability to stop and resume
-        - make more like 2D class, i.e. ability to explore boundaries
-
-        """
-        self.func = func
-        self.p_vals = p_vals
-        self.cores = cores
-        self.direc = direc
-        self.parallel = parallel
-
-    def single_eval(self, p_val):
-
-        # Run function
-        res = self.func(p_val)
-
-        # Save results
-        with open(self.direc + '/Res.csv', 'a') as f:
-            f.write("{:.12f}".format(p_val) + ',' + str(res) + '\n')
-
-    def run(self):
-
-        # Make results directory, if it doesn't already exist
-        if not os.path.isdir(self.direc):
-            os.mkdir(self.direc)
-
-        # Run
-        if self.parallel:
-            pool = multiprocessing.Pool(self.cores)
-            pool.map(self.single_eval, self.p_vals)
-        else:
-            for p in iter(self.p_vals):
-                self.single_eval(p)
+from typing import Callable
 
 
 class ParamSpace2D:
-    def __init__(self, func, p1_range, p2_range, resolution0, direc, resolution_step=2, n_iterations=1,
-                 explore_boundaries=True, parallel=False, cores=None, crange=None, cmap=None, save_fig=False, args=[],
+    def __init__(self,
+                 func: Callable,
+                 p1_range: tuple,
+                 p2_range: tuple,
+                 resolution0: int,
+                 path: str,
+                 resolution_step: int = 2,
+                 n_iterations: int = 1,
+                 explore_boundaries: bool = True,
+                 parallel: bool = False,
+                 cores: int = None,
+                 crange: tuple = None,
+                 cmap: str = None,
+                 save_fig: bool = False,
+                 args: tuple = (),
                  replace=False):
+
         """
 
-        Class to perform parameter space sweeps
+        Class to perform 2D parameter space sweeps
 
         Run by calling run function
         - performs analysis, saves results with each iteration to .csv files, and saves final figure
         - progress is saved, if interrupted can be resumed without loss by calling the run function again
 
-        Computation parameters
-        :param func: function - takes 2 parameters, returns an integer (must not be zero)
-        :param p1_range: range for parameter 1 (lower, upper)
-        :param p2_range: range for parameter 2 (lower, upper)
-        :param resolution0: n x n points on initial grid
-        :param resolution_step: how much resolution increases with each iteration
-        :param n_iterations: number of iterations
-        :param explore_boundaries: if True, will focus parameter search to regions where func result varies
-        :param parallel: if True, will run in parallel using number of cores specified
-        :param cores: number of cores on machine to use in parallel
-        :param args: additional arguments for func
-
-        # Saving parameters
-        :param direc: directory to save results. Directory must already exist
-
-        # Figure parameters
-        :param save_fig: if True, will save figure
-        :param crange: (lowest value, highest value)
-        :param cmap: if None, pyplot will use 'viridis'
-
-        To do:
-        - adjust so it can be run from multiple machines simultaneously
-        - test
-
+        Args:
+            func: function - takes 2 parameters, returns an integer or float (must not be zero)
+            p1_range: range for parameter 1 (lower, upper)
+            p2_range: range for parameter 2 (lower, upper)
+            resolution0: n x n points on initial grid
+            path: path to save results (will be created if it doesn't already exist)
+            resolution_step: how much resolution increases with each iteration
+            n_iterations: number of iterations
+            explore_boundaries: if True, will focus parameter search to regions where func result varies
+            parallel: if True, will run in parallel using number of cores specified
+            cores: number of cores on machine to use in parallel
+            crange: (lowest value, highest value)
+            cmap: if None, pyplot will use 'viridis'
+            save_fig: if True, will save figure
+            args: additional arguments for func
+            replace: if True, will replace any saved results in path, if False will load these and continue from where it left off
         """
 
         # Computation
@@ -112,7 +67,7 @@ class ParamSpace2D:
             self.cores = cores
 
         # Saving
-        self.direc = direc
+        self.path = path
         self.save_fig = save_fig
         self.replace = replace
 
@@ -138,7 +93,7 @@ class ParamSpace2D:
         state = self.func(*[float(i) for i in p1val_p2val.split(',')] + self.args)
 
         # Save state
-        with open(self.direc + '/' + str(self.iteration) + '.csv', 'a') as f:
+        with open(self.path + '/' + str(self.iteration) + '.csv', 'a') as f:
             f.write(p1val_p2val + ',' + str(state) + '\n')
 
     def batch_eval(self, pcombs):
@@ -160,7 +115,7 @@ class ParamSpace2D:
 
         """
 
-        with open(self.direc + '/' + str(self.iteration) + '.csv') as g:
+        with open(self.path + '/' + str(self.iteration) + '.csv') as g:
             for line in g:
                 p1, p2, val = line[:-1].split(',')
                 xind = ((float(p1) - self.p1_range[0]) * (self.n_sims - 1)) / (self.p1_range[1] - self.p1_range[0])
@@ -178,12 +133,12 @@ class ParamSpace2D:
 
         # Clear results directory if replace is True
         if self.replace:
-            if os.path.isdir(self.direc):
-                shutil.rmtree(self.direc)
+            if os.path.isdir(self.path):
+                shutil.rmtree(self.path)
 
         # Make results directory, if it doesn't already exist
-        if not os.path.isdir(self.direc):
-            os.mkdir(self.direc)
+        if not os.path.isdir(self.path):
+            os.mkdir(self.path)
 
         for iteration in range(self.n_iterations):
             print(iteration)
@@ -220,8 +175,8 @@ class ParamSpace2D:
             pcombs = ["{:.12f}".format(p1vals[i]) + ',' + "{:.12f}".format(p2vals[i]) for i in range(len(p1vals))]
 
             # Remove parameters already tested (if algorithm run before)
-            if os.path.isfile(self.direc + '/' + str(self.iteration) + '.csv'):
-                with open(self.direc + '/' + str(self.iteration) + '.csv') as f:
+            if os.path.isfile(self.path + '/' + str(self.iteration) + '.csv'):
+                with open(self.path + '/' + str(self.iteration) + '.csv') as f:
                     for line in f:
                         p = line.split(',')[0] + ',' + line.split(',')[1]
                         if p in pcombs:
@@ -229,8 +184,8 @@ class ParamSpace2D:
 
             # Carry over combinations from previous iteration
             if self.iteration != 0:
-                with open(self.direc + '/' + str(self.iteration - 1) + '.csv') as f:
-                    with open(self.direc + '/' + str(self.iteration) + '.csv', 'a') as g:
+                with open(self.path + '/' + str(self.iteration - 1) + '.csv') as f:
+                    with open(self.path + '/' + str(self.iteration) + '.csv', 'a') as g:
                         for line in f:
                             p = line.split(',')[0] + ',' + line.split(',')[1]
                             if p in pcombs:
@@ -285,7 +240,7 @@ class ParamSpace2D:
 
             # If no boundary (i.e. uniform parameter space), reload output from first iteration
             if np.sum(~np.isnan(self.res)) == 0:
-                with open(self.direc + '/0.csv') as f:
+                with open(self.path + '/0.csv') as f:
                     self.res[:, :] = int(f.readline().split(',')[2])
 
             # Else: Interpolate nans by flood fill algorithm
@@ -293,21 +248,21 @@ class ParamSpace2D:
                 o = np.argwhere(self.res != self.res)
                 while len(o) != 0:
                     pos = o[0]
-                    fillval = findzone(self.res, pos[0], pos[1])
+                    fillval = find_zone(self.res, pos[0], pos[1])
                     floodfill(self.res, pos[0], pos[1], fillval)
                     o = np.argwhere(self.res != self.res)
 
         # Save figure
         if self.save_fig:
             fig, ax = self.figure()
-            fig.savefig(self.direc + '/fig.png', dpi=300)
+            fig.savefig(self.path + '/fig.png', dpi=300)
             fig.close()
 
         # Specify int/float format
         f = (self.res % 1) == 0
 
         # Save row by row
-        with open(self.direc + '/Res.txt', 'w') as fh:
+        with open(self.path + '/Res.txt', 'w') as fh:
             for i, row in enumerate(self.res):
                 formats = f[i]
                 line = ' '.join(
@@ -335,51 +290,114 @@ class ParamSpace2D:
         return fig, ax
 
 
-def floodfill(array, x, y, newval):
+class ParamSpace1D:
+    def __init__(self,
+                 func: Callable,
+                 p_vals: np.ndarray,
+                 path: str,
+                 parallel: bool = False,
+                 cores: int = None):
+        """
+
+        Runs func with all combinations of p_vals, saves results to csv file
+        Experimental - use with caution
+
+        Args:
+            func: function, takes two parameter values, returns a float
+            p_vals: array of parameter values
+            path: directory to save results
+            parallel: if True, will run in parallel using number of cores specified
+            cores: number of cores to use, if parallel=True
+
+        To do:
+        - ability to stop and resume
+        - make more like 2D class, i.e. ability to explore boundaries
+
+        """
+
+        self.func = func
+        self.p_vals = p_vals
+        self.cores = cores
+        self.path = path
+        self.parallel = parallel
+
+    def single_eval(self, p_val):
+
+        # Run function
+        res = self.func(p_val)
+
+        # Save results
+        with open(self.path + '/Res.csv', 'a') as f:
+            f.write("{:.12f}".format(p_val) + ',' + str(res) + '\n')
+
+    def run(self):
+
+        # Make results directory, if it doesn't already exist
+        if not os.path.isdir(self.path):
+            os.mkdir(self.path)
+
+        # Run
+        if self.parallel:
+            pool = multiprocessing.Pool(self.cores)
+            pool.map(self.single_eval, self.p_vals)
+        else:
+            for p in iter(self.p_vals):
+                self.single_eval(p)
+
+
+def floodfill(array: np.ndarray, x: int, y: int, val: int):
     """
     Queue based flood fill algorithm
     Edits array in place
 
-    :param array:
-    :param x: x position
-    :param y: y position
-    :param newval: new value
-    :return:
+    To do: change to proper queue data structure instead of list
+
+    Args:
+        array: input array to fill (fills in place)
+        x: starting coordinate x position
+        y: starting coordinate y position
+        val: value to fill
+
+
     """
 
-    array[x, y] = newval
+    array[x, y] = val
     Q = [(x, y)]
     while len(Q) != 0:
         x, y = Q.pop(0)
 
         if x > 0:
             if np.isnan(array[x - 1, y]):
-                array[x - 1, y] = newval
+                array[x - 1, y] = val
                 Q.append((x - 1, y))
         if x < len(array[:, 0]) - 1:
             if np.isnan(array[x + 1, y]):
-                array[x + 1, y] = newval
+                array[x + 1, y] = val
                 Q.append((x + 1, y))
         if y > 0:
             if np.isnan(array[x, y - 1]):
-                array[x, y - 1] = newval
+                array[x, y - 1] = val
                 Q.append((x, y - 1))
         if y < len(array[0, :]) - 1:
             if np.isnan(array[x, y + 1]):
-                array[x, y + 1] = newval
+                array[x, y + 1] = val
                 Q.append((x, y + 1))
 
 
-def findzone(array, x, y):
+def find_zone(array: np.ndarray, x: int, y: int):
     """
     For 2D phase space class.
     Finds zone corresponding to a given location
     Based on flood fill algorithm
 
-    :param array:
-    :param x: x position
-    :param y: y position
-    :return: zone value
+    Args:
+        array: input array
+        x: x coordinate to find zone of
+        y: y coordinate to find zone of
+
+    Returns:
+        zone value
+
     """
 
     tested = np.zeros(array.shape)
